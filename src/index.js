d3ffa4d1f176472590bc9e9ae62d04be2e58858c
guobeibei <guobeibei@tal.com>
2021-12-22 18:37:32 +0800

    分析webpack打包出来的结果： 1. 通过es module导出，es module引入的方式 2. 通过commonjs导出，es module引入的方式

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 // commonjs 导入
-let title = require('./title.js');
-console.log(title);
-console.log(title.age);
+// let title = require('./title.js');
+// console.log(title);
+// console.log(title.age);
+
+// es module引入
+import name from "./title";
+console.log(name)
+
--- src/title.js
+++ src/title.js
@@ -2,6 +2,11 @@
 // exports.name = 'title_name';
 // exports.age = 'title_age';
 
+module.exports = {
+    name: 'title_name',
+    age: 'title_age'
+}
+
 // es modules导出
-export default 'title_name';
-export const age = 'title_age';
+// export default 'title_name';
+// export const age = 'title_age';
