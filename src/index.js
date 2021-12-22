c18f4cd75cf69b3bbf0e8e16041ee9e004a303da
guobeibei <guobeibei@tal.com>
2021-12-22 22:00:22 +0800

    懒加载：待实现

--- src/index.js
+++ src/index.js
@@ -4,6 +4,15 @@
 // console.log(title.age);
 
 // es module引入
-import name from "./title";
-console.log(name)
+// import name from "./title";
+// console.log(name)
+
+
+// 懒加载
+let load = document.getElementById('load');
+load.addEventListener('click', () => {
+    import(/* webpackChunkName: 'title' */'./title.js').then(res => {
+        console.log(res)
+    })
+})
 
